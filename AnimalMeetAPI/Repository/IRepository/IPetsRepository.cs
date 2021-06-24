fd805df393760f065496da84dac064602025c216
pvukovic6 <pvukovic@vub.hr>
2021-06-24 10:18:04 +0200

    Pets Repository and Interface added

---
+++ AnimalMeetAPI/Repository/IRepository/IPetsRepository.cs
@@ -0,0 +1,22 @@
+﻿using AnimalMeetAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalMeetAPI.Repository.IRepository
+{
+    public interface IPetsRepository
+    {
+        ICollection<Pets> GetPets();
+        ICollection<Pets> GetPetsInAnimalSubType(int animSubTypeId);
+        ICollection<Pets> GetPetsInUser(string userId);
+        Pets GetPets(int id);
+        bool PetsExist(string name);
+        bool PetsExist(int id);
+        bool CreatePets(Pets pets);
+        bool UpdatePets(Pets pets);
+        bool DeletePets(Pets pets);
+        bool Save();
+    }
+}
---
+++ AnimalMeetAPI/Repository/PetsRepository.cs
@@ -0,0 +1,73 @@
+﻿using AnimalMeetAPI.Data;
+using AnimalMeetAPI.Models;
+using AnimalMeetAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalMeetAPI.Repository
+{
+    public class PetsRepository : IPetsRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PetsRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreatePets(Pets pets)
+        {
+            _db.Pets.Add(pets);
+            return Save();
+        }
+
+        public bool DeletePets(Pets pets)
+        {
+            _db.Pets.Remove(pets);
+            return Save();
+        }
+
+        public ICollection<Pets> GetPets()
+        {
+            return _db.Pets.OrderBy(a => a.Name).ToList();
+        }
+
+        public Pets GetPets(int id)
+        {
+            return _db.Pets.FirstOrDefault(a => a.Id == id);
+        }
+
+        public ICollection<Pets> GetPetsInAnimalSubType(int animSubTypeId)
+        {
+            return _db.Pets.Include(c => c.AnimalSubtype).Where(c => c.AnimalSubtypeId == animSubTypeId).ToList();
+        }
+
+        public ICollection<Pets> GetPetsInUser(string userId)
+        {
+            return _db.Pets.Include(c => c.User).Where(c => c.UserId == userId).ToList();
+        }
+
+        public bool PetsExist(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PetsExist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdatePets(Pets pets)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
