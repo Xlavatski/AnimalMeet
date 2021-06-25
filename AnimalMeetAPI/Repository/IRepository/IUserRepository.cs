663ba745b9a9a1f8ced654c29fe2e437dc3621f1
pvukovic6 <pvukovic@vub.hr>
2021-06-25 10:03:51 +0200

    User Repository And Interface Added

---
+++ AnimalMeetAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using AnimalMeetAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalMeetAPI.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        ICollection<ApplicationUser> GetUsers();
+        ICollection<ApplicationUser> GetUsersInCity(int cityId);
+        ApplicationUser GetUser(int id);
+        bool UserExist(string name);
+        bool UserExist(int id);
+        bool CreateUser(ApplicationUser applicationUser);
+        bool UpdateUser(ApplicationUser applicationUser);
+        bool DeleteUser(ApplicationUser applicationUser);
+        bool Save();
+    }
+}
---
+++ AnimalMeetAPI/Repository/UserRepository.cs
@@ -0,0 +1,71 @@
+﻿using AnimalMeetAPI.Data;
+using AnimalMeetAPI.Models;
+using AnimalMeetAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalMeetAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreateUser(ApplicationUser applicationUser)
+        {
+            _db.ApplicationUsers.Add(applicationUser);
+            return Save();
+        }
+
+        public bool DeleteUser(ApplicationUser applicationUser)
+        {
+            _db.ApplicationUsers.Remove(applicationUser);
+            return Save();
+        }
+
+        public ApplicationUser GetUser(int id)
+        {
+            return _db.ApplicationUsers.Include(c => c.City).FirstOrDefault(a => a.Id == id);
+        }
+
+        public ICollection<ApplicationUser> GetUsers()
+        {
+            return _db.ApplicationUsers.Include(c => c.City).OrderBy(a => a.Name).ToList();
+        }
+
+        public bool UserExist(string name)
+        {
+            bool value = _db.ApplicationUsers.Any(a => a.Name.ToLower().Trim() == name.ToLower().Trim());
+            return value;
+        }
+
+        public bool UserExist(int id)
+        {
+            return _db.ApplicationUsers.Any(a => a.Id == id);
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateUser(ApplicationUser applicationUser)
+        {
+            _db.ApplicationUsers.Update(applicationUser);
+            return Save();
+        }
+
+        public ICollection<ApplicationUser> GetUsersInCity(int cityId)
+        {
+            return _db.ApplicationUsers.Include(c => c.City).Where(c => c.CityId == cityId).ToList();
+        }
+
+    }
+}
