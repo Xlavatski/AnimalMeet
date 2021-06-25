36faf82189bea4e34f2c374032ed0ad8db4dde4c
pvukovic6 <pvukovic@vub.hr>
2021-06-25 10:09:36 +0200

    Application User Create And Update Dto's Added

---
+++ AnimalMeetAPI/Models/Dtos/ApplicationUserUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalMeetAPI.Models.Dtos
+{
+    public class ApplicationUserUpdateDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public int? CityId { get; set; }
+        public string Role { get; set; }
+    }
+}
