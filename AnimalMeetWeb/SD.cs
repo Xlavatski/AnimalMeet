08b3aba5467ed9850746a564b65fadd581b3b671
pvukovic6 <pvukovic@vub.hr>
2021-06-28 11:31:32 +0200

    SD Added

---
+++ AnimalMeetWeb/SD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalMeetWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "http://localhost:56612/";
+        public static string AnimalSubTypeAPIPath = APIBaseUrl + "api/animalsubtype";
+        public static string AnimalTypeAPIPath = APIBaseUrl + "api/animaltype";
+        public static string AuthUserAPIPath = APIBaseUrl + "api/authuser";
+        public static string CityAPIPath = APIBaseUrl + "api/city";
+        public static string PetsAPIPath = APIBaseUrl + "api/pets";
+    }
+}
