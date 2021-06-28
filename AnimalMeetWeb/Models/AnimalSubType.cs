c82cced764930289ae27f046af45f86ce3344e46
pvukovic6 <pvukovic@vub.hr>
2021-06-28 13:49:12 +0200

    Models Added

---
+++ AnimalMeetWeb/Models/AnimalSubType.cs
@@ -0,0 +1,19 @@
+﻿using AnimalMeetAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalMeetWeb.Models
+{
+    public class AnimalSubType
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int? AnimalTypeId { get; set; }
+        public AnimalType AnimalType { get; set; }
+    }
+}
