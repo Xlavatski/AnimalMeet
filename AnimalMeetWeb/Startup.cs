be4fcbee2d56df8ab24d880a30d6c6ef33891835
pvukovic6 <pvukovic@vub.hr>
2021-06-28 13:48:31 +0200

    Update Startup.cs

--- AnimalMeetWeb/Startup.cs
+++ AnimalMeetWeb/Startup.cs
@@ -26,7 +26,7 @@ namespace AnimalMeetWeb
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddHttpClient();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
