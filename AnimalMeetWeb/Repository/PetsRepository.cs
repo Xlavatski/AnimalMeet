e247f7151ff408170ff6120a7f75d802a84a48a9
pvukovic6 <pvukovic@vub.hr>
2021-06-30 09:31:21 +0200

    Pets Repository And Interface Added

---
+++ AnimalMeetWeb/Repository/PetsRepository.cs
@@ -0,0 +1,22 @@
+﻿using AnimalMeetWeb.Models;
+using AnimalMeetWeb.Repository.IRepository;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalMeetWeb.Repository
+{
+    public class PetsRepository : Repository<Pets>, IPetsRepository
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public PetsRepository(IHttpClientFactory clientFactory): base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+    }
+}
