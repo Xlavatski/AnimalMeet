31226682d5e700b58fb98cadf9895b9089108eb9
pvukovic6 <pvukovic@vub.hr>
2021-06-30 08:31:24 +0200

    AnimalSubType Repository And Interface Added

---
+++ AnimalMeetWeb/Repository/AnimalSubTypeRepository.cs
@@ -0,0 +1,22 @@
+﻿using AnimalMeetWeb.Models;
+using AnimalMeetWeb.Repository.IRepository;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalMeetWeb.Repository
+{
+    public class AnimalSubTypeRepository : Repository<AnimalSubType>, IAnimalSubTypeRepository
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public AnimalSubTypeRepository(IHttpClientFactory clientFactory): base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+    }
+}
