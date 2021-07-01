f45ec0a32304243b7f8ab232d89a9619de5d46f0
dglavas <dglavas@DGLAVAS>
2021-07-01 08:45:46 +0200

    Added new desing

--- AnimalMeetWeb/Areas/Admin/Controllers/CityController.cs
+++ AnimalMeetWeb/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AnimalMeetWeb.Models;
+using AnimalMeetWeb.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,18 @@ using System.Threading.Tasks;
 
 namespace AnimalMeetWeb.Areas.Admin
 {
+    [Area("Admin")]
     public class CityController : Controller
     {
+        private readonly ICityRepository _cityRepository;
+
+        public CityController(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(new City() { });
         }
     }
 }
