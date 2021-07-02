da07f6fcb9c9798a56ea7f3b271497f0d431bb24
dglavas <dglavas@DGLAVAS>
2021-07-02 12:30:45 +0200

    Added new controller citiy methods and fix bugs

--- AnimalMeetWeb/Areas/Admin/Controllers/CityController.cs
+++ AnimalMeetWeb/Areas/Admin/Controllers/CityController.cs
@@ -1,5 +1,6 @@
 ﻿using AnimalMeetWeb.Models;
 using AnimalMeetWeb.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -21,5 +22,63 @@ namespace AnimalMeetWeb.Areas.Admin
         {
             return View(new City() { });
         }
+
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            City obj = new City();
+
+            if (id == null)
+            {
+                return View(obj);
+            }
+
+            obj = await _cityRepository.GetAsync(SD.CityAPIPath, id.GetValueOrDefault()/*, HttpContext.Session.GetString("JWToken")*/);
+
+            if (obj == null) 
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(City objC) 
+        {
+            if (ModelState.IsValid) 
+            {
+                if (objC.Id == 0)
+                {
+                    await _cityRepository.CreateAsync(SD.CityAPIPath, objC);
+                }
+                else 
+                {
+                    await _cityRepository.UpdateAsync(SD.CityAPIPath+objC.Id, objC);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(objC);
+        }
+
+        public async Task<IActionResult> GetAllCity() 
+        {
+            return Json(new { data = await _cityRepository.GetAllAsync(SD.CityAPIPath)});
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id) 
+        {
+            var status = await _cityRepository.DeleteAsync(SD.CityAPIPath, id);
+
+            if (status) 
+            {
+                return Json(new { success = true, message = "Delete Successful" });
+            }
+            return Json(new { success = false, message = "Delete Not Successful" });
+        }
+
     }
 }
--- AnimalMeetWeb/SD.cs
+++ AnimalMeetWeb/SD.cs
@@ -7,11 +7,11 @@ namespace AnimalMeetWeb
 {
     public static class SD
     {
-        public static string APIBaseUrl = "http://localhost:56612/";
-        public static string AnimalSubTypeAPIPath = APIBaseUrl + "api/animalsubtype";
-        public static string AnimalTypeAPIPath = APIBaseUrl + "api/animaltype";
-        public static string AuthUserAPIPath = APIBaseUrl + "api/authuser";
-        public static string CityAPIPath = APIBaseUrl + "api/city";
-        public static string PetsAPIPath = APIBaseUrl + "api/pets";
+        public static string APIBaseUrl = "http://localhost:56613/";
+        public static string AnimalSubTypeAPIPath = APIBaseUrl + "api/animalsubtype/";
+        public static string AnimalTypeAPIPath = APIBaseUrl + "api/animaltype/";
+        public static string AuthUserAPIPath = APIBaseUrl + "api/authuser/";
+        public static string CityAPIPath = APIBaseUrl + "api/city/";
+        public static string PetsAPIPath = APIBaseUrl + "api/pets/";
     }
 }
