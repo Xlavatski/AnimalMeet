bce81c9f127da8b621e2d3d55e1602444dc14666
pvukovic6 <pvukovic@vub.hr>
2021-07-06 11:21:16 +0200

    ViewModels Added

---
+++ AnimalMeetWeb/Models/ViewModel/IndexVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalMeetWeb.Models.ViewModel
+{
+    public class IndexVM
+    {
+        public IEnumerable<AnimalType> AnimalTypeList { get; set; }
+        public IEnumerable<AnimalSubType> AnimalSubTypeLsit { get; set; }
+    }
+}
